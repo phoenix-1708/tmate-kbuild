3afcee2dfae9bbf3f7aeaad8457a4f6d1fae9e11
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-12 22:05:55 +0530

    Create preparations.sh

---
+++ preparations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Preparations"
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install git gh bc build-essential zip curl libstdc++6 git wget python gcc clang libssl-dev openssl rsync flex bison gcc-aarch64-linux-gnu
+git config --global user.name "phoenix-1708"
+git config --global user.email "harikumar1708@gmail.com"
+git config --global color.ui true
+
+echo "Setting up Environment"
+git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
+
+echo "Cloning Sources"
+git clone https://github.com/phoenix-1708/epsilon.git -b master && cd epsilon
+
+echo "Build"
+bash kernel_setup.sh
