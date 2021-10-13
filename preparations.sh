3b18a856fa5af53f8d645874984afa863e16bbbc
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-13 20:22:16 +0530

    Update preparations.sh

--- preparations.sh
+++ preparations.sh
@@ -10,7 +10,11 @@ echo "Setting up Environment"
 git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
 
 echo "Cloning Sources"
-git clone https://github.com/phoenix-1708/epsilon.git -b master && cd epsilon
+git clone https://github.com/AdarshGrewal/android_kernel_xiaomi_sweet.git -b lineage-18.1 && cd android_kernel_xiaomi_sweet
+
+echo "copying necessary files"
+cp kernel_setup.sh /android_kernel_xiaomi_sweet/kernel_setup.sh
+cp kernel_build.sh /android_kernel_xiaomi_sweet/kernel_build.sh
 
 echo "Build"
 bash kernel_setup.sh
