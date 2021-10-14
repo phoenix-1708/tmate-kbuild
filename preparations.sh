6671004768b4d175ff40516c282878cac0574323
Harikumar <62538135+phoenix-1708@users.noreply.github.com>
2021-10-14 11:45:30 +0530

    Update preparations.sh

--- preparations.sh
+++ preparations.sh
@@ -10,11 +10,14 @@ echo "Setting up Environment"
 git clone https://github.com/akhilnarang/scripts bscripts && cd bscripts && bash setup/android_build_env.sh && cd ..
 
 echo "Cloning Sources"
-git clone https://github.com/AdarshGrewal/android_kernel_xiaomi_sweet.git -b lineage-18.1 && cd android_kernel_xiaomi_sweet
+git clone https://github.com/AdarshGrewal/android_kernel_xiaomi_sweet.git -b lineage-18.1
 
 echo "copying necessary files"
-sudo cp kernel_setup.sh /android_kernel_xiaomi_sweet/kernel_setup.sh
-sudo cp kernel_build.sh /android_kernel_xiaomi_sweet/kernel_build.sh
+cp kernel_setup.sh /android_kernel_xiaomi_sweet/kernel_setup.sh
+cp kernel_build.sh /android_kernel_xiaomi_sweet/kernel_build.sh
+
+echo "changing Directory to android_kernel_xiaomi_sweet"
+cd android_kernel_xiaomi_sweet
 
 echo "Build"
 bash kernel_setup.sh
